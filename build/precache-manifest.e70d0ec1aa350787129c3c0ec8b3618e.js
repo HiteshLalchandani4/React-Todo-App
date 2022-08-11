c3bf3b1e047e24ed9214b4ecdca20a84d4d69ddf
HiteshLalchandani4 <111034014+HiteshLalchandani4@users.noreply.github.com>
2022-08-11 09:19:50 +0530

    Add files via upload

---
+++ build/precache-manifest.e70d0ec1aa350787129c3c0ec8b3618e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38260e03536d0e4e5818",
+    "url": "/static/css/main.6e487dc0.chunk.css"
+  },
+  {
+    "revision": "38260e03536d0e4e5818",
+    "url": "/static/js/main.38260e03.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "ca98ef9ff6088d7d43b9",
+    "url": "/static/js/2.ca98ef9f.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "82dbb9651073125828567564ace52b94",
+    "url": "/index.html"
+  }
+];
